55fa443b59c24281a996db07680f63ba6232107d
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-03-27 03:34:17 +0000

    tercero

---
+++ ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a, b, area;
+    cout<<"Calcular el area de un rectangulo"<<endl;
+    cout<<"ingrese base"<<endl;
+    cin>>b;
+    cout<<"ingrese altura"<<endl;
+    cin>>a;
+    area=b*a;
+    cout<<"el Area es: "<<endl;
+    return 0;
+}
