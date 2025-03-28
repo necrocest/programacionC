3aec97b54655bcce2e8aa407c65bc8a6155d05eb
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-03-28 15:48:04 +0000

    estructuras modulares

---
+++ funciones.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+float resis(float v, float a){
+    return (v/a);
+};
+int main(){
+    float v, a, resis;
+    cout<<"ingreses V"<<endl;
+    cin>>v;
+    cout<<"ingrese I"<<endl;
+    cin>>a;
+    cout<<resis<<endl;
+return 0;
+}
