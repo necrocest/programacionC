216c52dacb8e0fcc40d52c651b724741f534253f
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-04-22 16:02:59 +0000

    vectores en c++

---
+++ vectores.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Arreglos de una dimension"<<endl;
+	char vocales[5];
+	vocales[0] = 'a';
+	vocales[1] = 'e';
+	vocales[2] = 'i';
+	vocales[3] = 'o';
+	vocales[4] = 'u';
+	cout<<"vocales[4] = "<<vocales[4]<<endl;
+	return 0;
+}
