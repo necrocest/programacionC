940a5b408bf00d01cbe7bd2b70f061dccd957cca
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-02 17:00:07 +0000

    ejercicios de clase: vectores

--- vectores o arrays/ejercicio5.cpp
+++ vectores o arrays/ejercicio5.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int par, a;
+    int a = 0;
     int vector[7] = {2,3,8,18,10,30,20};
-    int vector1[vector[7]];
     for(int i = 1; i < 7; i+=2){
             cout<<"en el lugar: "<<i<<" se encuentra: "<<vector[i]<<endl;
-    }
+            vector[i] = a;
+            a++;
+        }
+    cout<<a<<endl;
     /*nuevo vector*/
-    int vector_nuevo[3];
-    int j = 0;
+    int vector_nuevo[a];
     int i = 0;
+    
     for(int j = 1; j < 7; j+=2){
         vector_nuevo[j] = vector[i];
     }
     /*imprimir nuevo vector*/
-    for(int i = 0; i < 3; i++){
+    for(int b = 0; b < a; b++){
         cout<<vector_nuevo[i]<<endl;
     }
     return 0;
