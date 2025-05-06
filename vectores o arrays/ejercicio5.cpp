bb2ac7c78c29a7191c35880a673f77f9515368ed
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-06 15:56:23 +0000

    ejercicios de clase: vectores: pasar de vector a vector

--- vectores o arrays/ejercicio5.cpp
+++ vectores o arrays/ejercicio5.cpp
@@ -1,24 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a = 0;
+    int a = 0, indice = 0, b = 0;
     int vector[7] = {2,3,8,18,10,30,20};
     for(int i = 1; i < 7; i+=2){
             cout<<"en el lugar: "<<i<<" se encuentra: "<<vector[i]<<endl;
-            vector[i] = a;
             a++;
-        }
-    cout<<a<<endl;
-    /*nuevo vector*/
+        } 
     int vector_nuevo[a];
-    int i = 0;
-    
     for(int j = 1; j < 7; j+=2){
-        vector_nuevo[j] = vector[i];
+        vector_nuevo[indice] = vector[j];
+        indice++;
     }
-    /*imprimir nuevo vector*/
     for(int b = 0; b < a; b++){
-        cout<<vector_nuevo[i]<<endl;
+        cout<<vector_nuevo[b]<<endl;    
     }
+
     return 0;
 }
