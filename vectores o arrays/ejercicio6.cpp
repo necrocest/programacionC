dd22b835975447f9e446a21ef1069f57c5495cbb
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-06 16:41:59 +0000

    ejercicios de clase: vectores

---
+++ vectores o arrays/ejercicio6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a; 
+    int multiplicacion = 0, division = 0, suma = 0;
+    int total = 0;
+    int vector[3] = {80,90,70};
+    int vector2[3] = {2, 3, 1};
+    for(int i = 0; i < 3; i++){
+        multiplicacion = (vector[i] * vector2[i]);
+        total+=multiplicacion;
+    }for( int j = 0; j < 3; j++){
+        suma += vector2[j];
+        division = total / suma; 
+    }
+    cout<<"El total es: "<<division<<endl;
+    return 0;
+}
