3709724dca3ed18413177c56a5b64f97de853875
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-08 01:20:30 +0000

    ejercicios de clase: vectores

--- vectores o arrays/ejercicio7.cpp
+++ vectores o arrays/ejercicio7.cpp
@@ -4,22 +4,25 @@ int main(){
     int a; 
     int multiplicacion = 0, division = 0, suma = 0;
     int total = 0, n, n1;
-    int vector[n];    
-    int vector2[n1];
     cout<<"cuantos elementos en el vector 1:"<<endl;
     cin>>n;
+    int vector[n];
     cout<<"ingrese los elementos del vector 2(debe ser igual al vector 1): "<<endl;
     cin>>n1;
+    int vector2[n1];
     for(int i = 0; i < 3; i++){
         cout<<"ingrese los elementos del vector"<<endl;
-        cin>>vector[n];
+        cin>>vector[i];
     }
     for(int i = 0; i < 3; i++){
         cout<<"ingrese los elementos del vector 2"<<endl;
-        cin>>vector[n1];
+        cin>>vector2[i];
+    }
+    for(int i = 0; i < 3; i++){
         multiplicacion = (vector[i] * vector2[i]);
         total+=multiplicacion;
-    }for( int j = 0; j < 3; j++){
+    }
+    for( int j = 0; j < 3; j++){
         suma += vector2[j];
         division = total / suma; 
     }
