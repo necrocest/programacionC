b59f3f5ca300e4d1015ee82bb4fcadb766fd31ab
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-15 03:02:53 +0000

    ejercicios vectores

--- vectores o arrays/ejercicio17.cpp
+++ vectores o arrays/ejercicio17.cpp
@@ -1,1 +1,17 @@
 /*Desarrolle un programa que lea un vector de enteros y cuente cuántos números pares contiene.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"ingresa la cantidad de elementos en el arreglo: "; cin>>x;
+    int array[x];
+    for(int i = 0; i < x; i++){
+        cout<<"ingrese los numeros del arreglo: "; cin>>array[i];
+    }
+    for(int j = 0; j < x; j++){
+        if(array[j]%2 == 0){
+            cout<<"En la posicion "<<j<<" se encuentra el numero "<<array[j]<<endl;
+        }
+    }
+    return 0;
+}
