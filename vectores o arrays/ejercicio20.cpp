2f3e325be1d927366a6dba2d63761332f2b24235
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-17 15:44:52 +0000

    ejercicios vectores

--- vectores o arrays/ejercicio20.cpp
+++ vectores o arrays/ejercicio20.cpp
@@ -9,12 +9,14 @@ quire:
 #include<iostream>
 using namespace std;
 int main(){
-    int total = 0, a = 0, b = 0, menor = 0, mayor = 0;
+    int total = 0, a = 0, b = 0;
     int lista_compras[3];
     for(int i = 0; i < 3; i++){
         cout<<"agregue el precio del producto: "; cin>>lista_compras[i];
         total = total + lista_compras[i];
     }
+    int mayor = lista_compras[0];
+    int menor = lista_compras[0];
     for(int j = 0; j < 3; j++){
         if(lista_compras[j] > 100000){
             a++;
@@ -22,21 +24,19 @@ int main(){
         if(lista_compras[j] < 100000){
             b++;
         }
-        
-    }
-    for(int c = 0; c < 3; c++){
-        if(lista_compras[c]>mayor){
-            mayor=lista_compras[c];
+
+        if(lista_compras[j]>mayor){
+            mayor=lista_compras[j];
         }
         else {
-            mayor=lista_compras[c];
+            menor=lista_compras[j];
         }
+        
     }
     cout<<"el producto "<<a<<" cuesta mas de 100k"<<endl;
     cout<<"el producto "<<b<<" cuesta menos de 100k"<<endl;
     cout<<"producto mas caro "<<mayor<<endl;
     cout<<"producto mas barato "<<menor<<endl;
-    
     cout<<total<<endl;
     return 0;
 }
