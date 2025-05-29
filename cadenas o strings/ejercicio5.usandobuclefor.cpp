1a8208ae0778e36ba8c90d59e13c97ae375ba6f8
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-29 04:37:39 +0000

    caracteres o cadenas

--- cadenas o strings/ejercicio5.usandobuclefor.cpp
+++ cadenas o strings/ejercicio5.usandobuclefor.cpp
@@ -9,12 +9,19 @@ int main(){
     cout<<"introduzca una palabra: "; 
     cin.getline(palabra,20,'\n');
     strcpy(palabra1, palabra);
-    
+    /*inicializamos "len" entero para poder saber la dimension de la palabra
+    y con eso poder inicializar el bucle for*/
+    int len = strlen(palabra1);
+    /*se inicializa len/2 porque solo se necesita analizar la mitad de la palabra*/
+    for(int i = 0; i < len / 2; i++){
+        char caracter_actual = palabra1[i]; /*capturar las iteraciones (almacen temporal i)*/
+        palabra1[i] = palabra1[len-i-1]; /*se reemplaza el inicio con el final*/
+        palabra1[len-i-1] = caracter_actual; /*se reemplaza el fianl con el inicio (se guarda en caracter actual)*/
+    }
+    cout<<"Palabra invertida: "<<palabra1<<"\n";
     if(strcmp(palabra,palabra1)==0){
-        cout<<"es palindroma";
-    }else{
-        cout<<"no es palindora";
-    } 
+        cout<<"la palabra "<<palabra1<<" es palindroma";
+    }
     cout<<"\n";
     return 0;
 }
