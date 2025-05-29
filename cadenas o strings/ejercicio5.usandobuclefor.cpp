c19a2b1c9d3bd7e347d10b19a65dba2dcc92e340
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-05-29 04:00:06 +0000

    caracteres o cadenas

---
+++ cadenas o strings/ejercicio5.usandobuclefor.cpp
@@ -0,0 +1,20 @@
+/*Hacer un programa que determine si una palabra es palíndroma*/
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char palabra [20];
+    char palabra1 [20];
+    cout<<"introduzca una palabra: "; 
+    cin.getline(palabra,20,'\n');
+    strcpy(palabra1, palabra);
+    
+    if(strcmp(palabra,palabra1)==0){
+        cout<<"es palindroma";
+    }else{
+        cout<<"no es palindora";
+    } 
+    cout<<"\n";
+    return 0;
+}
