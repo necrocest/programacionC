fb94bc6029d7361bb1f9c4b55738c3b3e0885f85
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-06-05 23:08:18 +0000

    trabajo final

--- proyectodeprogramacionc ++/codigo.cpp
+++ proyectodeprogramacionc ++/codigo.cpp
@@ -46,19 +46,33 @@ int main(){
             }
             numeroCuentas[totalCuentas] = nuevoNumero;
 
-            /*validar nombre del titular*/
+            /*validar nombte del titular*/
             cout<<"Ingrese el nombre del titular de la cuenta: "<<endl;
-            getline(cin,titulares[totalCuentas], '\n');
-            
+            cin.ignore();
+            getline(cin,titulares[totalCuentas],'\n');
             /*tipo de cuenta*/
-            cout<<"Tipo de cuenta: "<<endl;
-            getline(cin,tipoCuenta[totalCuentas],'\n');
+            cout<<"Ingrese el tiepo de cuenta(ahorros o corriente)"<<endl;
+            getline(cin, tipoCuenta[totalCuentas]);
             /*se inicia en 0 porque esta recien creada*/
             saldo[totalCuentas] = 0.0;
             /*contador para almacenar las cuentas creadas*/
             totalCuentas++;
-            cout<<"cuenta creada exitosamente"<<endl;
+            cout<<"Cuenta creada exitosamente"<<endl;
+            cout<<"Bienvenido al Banco WFM"<<endl;
+        }else if(opcion == 2){
+            /*Listar cuentas*/
+            for(int i = 0; i < totalCuentas; i++){
+                cout<<"\n";
+                cout<<"Nombre del titular: "<<titulares[i];
+                cout<<"\nNumero de la cuenta: "<<numeroCuentas[i];
+                cout<<"\ntipo de cuenta: "<<tipoCuenta[i];
+                cout<<"\nsaldo de la cuenta: "<<saldo[i]<<endl;
+            }
+            cout<<"\n";
+        }else if(opcion == 3){
+            
         }
+        
     }
     return 0;
 }
