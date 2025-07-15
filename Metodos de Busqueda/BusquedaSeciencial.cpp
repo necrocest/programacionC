01e56a70c8bb936dcab14eee4fcb7bbc377a79ed
Williamf40 <63164108+necrocest@users.noreply.github.com>
2025-07-15 22:24:43 +0000

    metodos de busqueda Secuencial - Binaria

---
+++ Metodos de Busqueda/BusquedaSeciencial.cpp
@@ -0,0 +1,24 @@
+/*a[5] = {1,2,3,4,5}*/
+/*tambien se puede con caracteres*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a[5] = {1,2,3,4,5};
+    int i, dato;
+    bool band = false;
+    cout << "Ingrese el dato a buscar: ";
+    cin >> dato;
+    i=0;
+    while((i < 5) && (band == false)){
+        if(a[i] == dato){
+            band = true;
+        }
+        i++;
+    }
+    if(band == false){
+        cout << "El dato no se encuentra en el arreglo." << endl;
+    } else {
+        cout << "El dato se encuentra en el arreglo." << endl;  
+    }
+}
